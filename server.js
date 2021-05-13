205b9fc1b575c4078951995c631c3083222133a1
fuad <fuad.gyulmamedov.2001@mail.ru>
2021-05-13 23:08:48 +0400

    test

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const JsonServer =  require('json-server')
-const server = JsonServer.create()
-const  router =  JsonServer.router('./public/pizza.json')
+const JsonServer =  require('json-server');
+const server = JsonServer.create();
+const  router =  JsonServer.router('./public/pizza.json');
 const middlewares  =  JsonServer.defaults({
     static: './build',
-})
+});
 
-const  PORT = process.env.PORT  || 3001
+const  PORT = process.env.PORT  || 3001;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
 server.listen(PORT,  ()  =>  {
     console.log('Server is running')
